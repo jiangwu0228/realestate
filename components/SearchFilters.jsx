6ccc029048c4664be24fd7156e19b6d4540458c8
Jason Wu <jiangwu0228@gmail.com>
2022-01-27 13:44:09 +1300

    Buy and Rent tab done
    Actually it's implement by search for-rent  or for-sale

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { MdCancel } from 'react-icons/md';
+import Image from 'next/image';
+
+const Searchfilters = () => {
+    return (
+        <div>
+            <h1>Search Filters</h1>
+        </div>
+    );
+}
+
+export default Searchfilters;
